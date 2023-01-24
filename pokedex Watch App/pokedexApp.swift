ad512606cad32ad5b91c661ca989296f5b80d9f4
danialepaco <danialepaco@hotmail.com>
2023-01-24 14:06:03 -0500

    Initial Commit

---
+++ pokedex Watch App/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex Watch App
+//
+//  Created by Daniel Parra on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
